530733285e503b4105d3d900629156856d99c978
Shaxzodbek <shaxzodbek.qambaraliyev@gmail.com>
2021-12-09 11:17:43 +0500

    payment

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+//handle requests from payment system
+Route::any('/handle/{paysys}', function ($paysys) {
+    (new Goodoneuz\PayUz\PayUz)->driver($paysys)->handle();
+});
+
+//redirect to payment system or payment form
+Route::any('/pay/{paysys}/{key}/{amount}', function ($paysys, $key, $amount) {
+    $model = Goodoneuz\PayUz\Services\PaymentService::convertKeyToModel($key);
+    $url = request('redirect_url', '/'); // redirect url after payment completed
+    $pay_uz = new Goodoneuz\PayUz\PayUz;
+    $pay_uz
+        ->driver($paysys)
+        ->redirect($model, $amount, 860, $url);
 });
