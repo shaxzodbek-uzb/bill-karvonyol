741930dae2c988f1e2eb8c87cbd74b1ec9e36f31
Shaxzodbek <shaxzodbek.qambaraliyev@gmail.com>
2021-12-09 11:37:24 +0500

    models

---
+++ app/KarvonOrder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class KarvonOrder extends Model
+{
+    protected $table = 'siparisler';
+    protected $fillable = ['ID', 'UYEID', 'STOKID', 'BIRIM', 'MIKTAR', 'FIYAT', 'KDV', 'ISKONTO', 'TUTAR', 'ZAMAN', 'DURUM', 'KREDINO', 'KREDITUTAR', 'TAHSILATNO'];
+    // ID 	UYEID 	STOKID 	BIRIM 	MIKTAR 	FIYAT 	KDV 	ISKONTO 	TUTAR 	ZAMAN 	DURUM 	KREDINO 	KREDITUTAR 	TAHSILATNO
+    // 99 	1 	297 	Adet 	1.00 	84.99 	8 	0.00 	84.99 	1636630519 	1 	0 	0.00 	123445566?
+}
---
+++ app/KarvonTransaction.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class KarvonTransaction extends Model
+{
+    protected $table = 'tahsilatlar';
+    protected $fillable = ['ID', 'UYEID', 'SIPARISID', 'SIPARISTUTAR', 'TUTAR', 'DEKONDNO', 'ODEMEBILGISI', 'ZAMAN', 'TARIH', 'SAAT', 'DURUM'];
+    // UYEID 	SIPARISID 	SIPARISTUTAR 	TUTAR 	DEKONDNO 	ODEMEBILGISI 	ZAMAN 	TARIH 	SAAT 	DURUM
+    // 5 	1 	0 	91.79 	91.79 	123445566 	223345 	1636630519 	2021-11-11 	14:35 	1
+}
