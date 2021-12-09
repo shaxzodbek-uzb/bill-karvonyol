79224a49e6a87ac35e11cdc6ba4fb3e93b37c79c
Shaxzodbek <shaxzodbek.qambaraliyev@gmail.com>
2021-12-09 12:41:25 +0500

    fixes

--- app/KarvonOrder.php
+++ app/KarvonOrder.php
@@ -10,4 +10,5 @@ class KarvonOrder extends Model
     protected $fillable = ['id', 'UYEID', 'STOKID', 'BIRIM', 'MIKTAR', 'FIYAT', 'KDV', 'ISKONTO', 'TUTAR', 'ZAMAN', 'DURUM', 'KREDINO', 'KREDITUTAR', 'TAHSILATNO'];
     // ID 	UYEID 	STOKID 	BIRIM 	MIKTAR 	FIYAT 	KDV 	ISKONTO 	TUTAR 	ZAMAN 	DURUM 	KREDINO 	KREDITUTAR 	TAHSILATNO
     // 99 	1 	297 	Adet 	1.00 	84.99 	8 	0.00 	84.99 	1636630519 	1 	0 	0.00 	123445566?
+    public $timestamps = false;
 }
--- app/KarvonTransaction.php
+++ app/KarvonTransaction.php
@@ -10,4 +10,5 @@ class KarvonTransaction extends Model
     protected $fillable = ['id', 'UYEID', 'SIPARISID', 'SIPARISTUTAR', 'TUTAR', 'DEKONDNO', 'ODEMEBILGISI', 'ZAMAN', 'TARIH', 'SAAT', 'DURUM'];
     // UYEID 	SIPARISID 	SIPARISTUTAR 	TUTAR 	DEKONDNO 	ODEMEBILGISI 	ZAMAN 	TARIH 	SAAT 	DURUM
     // 5 	1 	0 	91.79 	91.79 	123445566 	223345 	1636630519 	2021-11-11 	14:35 	1
+    public $timestamps = false;
 }
